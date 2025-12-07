32e6ca55cf8943d135e59ba8e70a01c1c58dd9b1
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-12-07 19:59:50 +0800

    merge error fix2

---
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -0,0 +1,18 @@
+package labxx.sict.must.edu.mn;
+
+/**
+ * Үржүүлэх үйлдлийг гүйцэтгэдэг класс.
+ */
+public class Multiplication {
+    
+    /**
+     * Хоёр тоог үржүүлнэ.
+     *
+     * @param a эхний тоо
+     * @param b хоёрдугаар тоо
+     * @return үржвэр
+     */
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
